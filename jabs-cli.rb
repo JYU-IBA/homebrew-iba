8c9a912794cb814e51fa2f94e799a73256e184f4
Jaakko Julin <jaakko.julin@jyu.fi>
2022-04-12 15:33:12 +0300

    JaBS head was wrong, since the main branch is main.

--- jabs-cli.rb
+++ jabs-cli.rb
@@ -3,7 +3,7 @@ class JabsCli < Formula
   homepage "https://github.com/JYU-IBA/jabs/"
   url "https://github.com/JYU-IBA/jabs/archive/v0.6.1.tar.gz"
   sha256 "2ac98f2e329a03e56f791a456006671f09e32681336a781803485676db4c5be4"
-  head "https://github.com/JYU-IBA/jabs.git"
+  head "https://github.com/JYU-IBA/jabs.git", branch: "main"
   depends_on "cmake" => :build
   depends_on "gsl"
   depends_on "jibal"
