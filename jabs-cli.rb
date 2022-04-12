65dd5242d52c581790310a51c84272496e73e647
Jaakko Julin <jaakko.julin@jyu.fi>
2022-04-12 15:48:11 +0300

    JaBS sha256 was wrong.

--- jabs-cli.rb
+++ jabs-cli.rb
@@ -2,7 +2,7 @@ class JabsCli < Formula
   desc "Jaakko's Backscattering Simulator (JaBS) command-line version"
   homepage "https://github.com/JYU-IBA/jabs/"
   url "https://github.com/JYU-IBA/jabs/archive/v0.6.1.tar.gz"
-  sha256 "2ac98f2e329a03e56f791a456006671f09e32681336a781803485676db4c5be4"
+  sha256 "e62bc8c9d55a60aa24ebf71e484f34940aea02e8c7d5f8161ecbf468968d35b3"
   head "https://github.com/JYU-IBA/jabs.git", branch: "main"
   depends_on "cmake" => :build
   depends_on "gsl"
