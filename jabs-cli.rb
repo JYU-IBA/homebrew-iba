de558bd2c80722787d6986d26666ec111d89e730
Jaakko Julin <jaakko.julin@jyu.fi>
2022-04-29 20:11:23 +0300

    jabs-cli 0.6.4

--- jabs-cli.rb
+++ jabs-cli.rb
@@ -1,8 +1,8 @@
 class JabsCli < Formula
   desc "Jaakko's Backscattering Simulator (JaBS) command-line version"
   homepage "https://github.com/JYU-IBA/jabs/"
-  url "https://github.com/JYU-IBA/jabs/archive/v0.6.3.tar.gz"
-  sha256 "f1ab0fa3571fe792d6fab0b71fc62789e00ae27c06eee7ff3507c3fc0a00fa19"
+  url "https://github.com/JYU-IBA/jabs/archive/v0.6.4.tar.gz"
+  sha256 "13ab9302a336d39611e6f629357b8d833a1ce6df74224d1236c1fa8c16738c67"
   head "https://github.com/JYU-IBA/jabs.git", branch: "main"
   depends_on "cmake" => :build
   depends_on "gsl"
