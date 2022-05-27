b4083197df9d82dd9dd3887e7689dca14f50f704
Jaakko Julin <jaakko.julin@jyu.fi>
2022-05-27 15:57:36 +0300

    jibal 0.3.8

--- jibal.rb
+++ jibal.rb
@@ -1,8 +1,8 @@
 class Jibal < Formula
   desc "Jyväskylä ion beam analysis library (JIBAL)"
   homepage "https://github.com/JYU-IBA/jibal/"
-  url "https://github.com/JYU-IBA/jibal/archive/v0.3.7.tar.gz"
-  sha256 "453b4c9ddb7010a4c0c870a4d9170b85a258d49efd820a4b25bfc69e3e86347b"
+  url "https://github.com/JYU-IBA/jibal/archive/v0.3.8.tar.gz"
+  sha256 "7799c2b0d3a743be8f0bb5c300923a3463864b3260bcab39bc4525f512ac1660"
   head "https://github.com/JYU-IBA/jibal.git"
   depends_on "cmake" => :build
   depends_on "gsl"
