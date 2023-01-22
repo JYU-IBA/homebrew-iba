5522115cf31646e383aa0ffc92026b9d03817e3f
Jaakko Julin <jaakko.julin@jyu.fi>
2023-01-22 18:20:02 +0200

    jabs-cli includes libxml2, version 0.6.7

--- jabs-cli.rb
+++ jabs-cli.rb
@@ -1,12 +1,13 @@
 class JabsCli < Formula
   desc "Jaakko's Backscattering Simulator (JaBS) command-line version"
   homepage "https://github.com/JYU-IBA/jabs/"
-  url "https://github.com/JYU-IBA/jabs/archive/v0.6.1.tar.gz"
-  sha256 "e62bc8c9d55a60aa24ebf71e484f34940aea02e8c7d5f8161ecbf468968d35b3"
+  url "https://github.com/JYU-IBA/jabs/archive/v0.6.7.tar.gz"
+  sha256 "6d72115fb7f23d28f7c60e2a442009c7e6b762c6c9d0071fff30a94924ee6659"
   head "https://github.com/JYU-IBA/jabs.git", branch: "main"
   depends_on "cmake" => :build
   depends_on "gsl"
   depends_on "jibal"
+  depends_on "libxml2"
 
   def install
     system "cmake", ".", *std_cmake_args
